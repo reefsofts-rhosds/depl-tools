df247f79dd042039ad721892f979c8a5b44ddea1
reefsofts-rhosds <youri.lkh@girofle.org>
2025-07-06 13:39:31 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -13,7 +13,7 @@ def run_command(command):
 # Unmount the share if it's already mounted
 run_command(["sudo", "umount", "/depl-share"])
 
-# Mount the remote share
+# Mount the remote share and show the menu
 if run_command(["sudo", "curlftpfs", "depl-share-access:depl-share-access@192.168.1.27:/", "/depl-share"]):
     subprocess.run(["clear"])
     print("Depl-Tools")
@@ -22,7 +22,7 @@ if run_command(["sudo", "curlftpfs", "depl-share-access:depl-share-access@192.16
     print("1. Create image of this computer")
     print("2. Apply an image onto this computer")
     print("3. Use Finnix Shell")
-
+    # Action execution
     chosen_action = input("Choose what action you want to perform: ")
     if chosen_action == "1":
         print("Launching Image Creation Tool")
@@ -33,4 +33,5 @@ if run_command(["sudo", "curlftpfs", "depl-share-access:depl-share-access@192.16
     elif chosen_action == "3":
         print("Launching Finnix Shell")
 else:
+    # Error handling
     print("Failed to mount the remote share. Please check the error messages above.")
